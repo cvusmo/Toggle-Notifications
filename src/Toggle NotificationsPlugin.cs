6524699fa33a019c69bf443e0694312d52aaf092
blacksheepcosmo <82072837+blacksheepcosmo@users.noreply.github.com>
2023-05-03 22:07:46 -0500

    Add files via upload

---
+++ src/Toggle NotificationsPlugin.cs
@@ -0,0 +1,118 @@
+﻿using BepInEx;
+using HarmonyLib;
+using KSP.UI.Binding;
+using SpaceWarp;
+using SpaceWarp.API.Assets;
+using SpaceWarp.API.Mods;
+using SpaceWarp.API.Game;
+using SpaceWarp.API.Game.Extensions;
+using SpaceWarp.API.UI;
+using SpaceWarp.API.UI.Appbar;
+using UnityEngine;
+
+namespace Toggle_Notifications;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
+public class Toggle_NotificationsPlugin : BaseSpaceWarpPlugin
+{
+    // These are useful in case some other mod wants to add a dependency to this one
+    public const string ModGuid = MyPluginInfo.PLUGIN_GUID;
+    public const string ModName = MyPluginInfo.PLUGIN_NAME;
+    public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
+    
+    private bool _isWindowOpen;
+    private Rect _windowRect;
+
+    private const string ToolbarFlightButtonID = "BTN-Toggle_NotificationsFlight";
+    private const string ToolbarOABButtonID = "BTN-Toggle_NotificationsOAB";
+
+    public static Toggle_NotificationsPlugin Instance { get; set; }
+
+    /// <summary>
+    /// Runs when the mod is first initialized.
+    /// </summary>
+    public override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Instance = this;
+
+        // Register Flight AppBar button
+        Appbar.RegisterAppButton(
+            "Toggle Notifications",
+            ToolbarFlightButtonID,
+            AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
+            isOpen =>
+            {
+                _isWindowOpen = isOpen;
+                GameObject.Find(ToolbarFlightButtonID)?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(isOpen);
+            }
+        );
+
+        // Register OAB AppBar Button
+        Appbar.RegisterOABAppButton(
+            "Toggle Notifications",
+            ToolbarOABButtonID,
+            AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
+            isOpen =>
+            {
+                _isWindowOpen = isOpen;
+                GameObject.Find(ToolbarOABButtonID)?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(isOpen);
+            }
+        );
+
+        // Register all Harmony patches in the project
+        Harmony.CreateAndPatchAll(typeof(Toggle_NotificationsPlugin).Assembly);
+
+        // Try to get the currently active vessel, set its throttle to 100% and toggle on the landing gear
+        try
+        {
+            var currentVessel = Vehicle.ActiveVesselVehicle;
+            if (currentVessel != null)
+            {
+                currentVessel.SetMainThrottle(1.0f);
+                currentVessel.SetGearState(true);
+            }
+        }
+        catch (Exception e) {}
+        
+        // Fetch a configuration value or create a default one if it does not exist
+        var defaultValue = "my_value";
+        var configValue = Config.Bind<string>("Settings section", "Option 1", defaultValue, "Option description");
+        
+        // Log the config value into <KSP2 Root>/BepInEx/LogOutput.log
+        Logger.LogInfo($"Option 1: {configValue.Value}");
+    }
+
+    /// <summary>
+    /// Draws a simple UI window when <code>this._isWindowOpen</code> is set to <code>true</code>.
+    /// </summary>
+    private void OnGUI()
+    {
+        // Set the UI
+        GUI.skin = Skins.ConsoleSkin;
+
+        if (_isWindowOpen)
+        {
+            _windowRect = GUILayout.Window(
+                GUIUtility.GetControlID(FocusType.Passive),
+                _windowRect,
+                FillWindow,
+                "Toggle Notifications",
+                GUILayout.Height(350),
+                GUILayout.Width(350)
+            );
+        }
+    }
+
+    /// <summary>
+    /// Defines the content of the UI window drawn in the <code>OnGui</code> method.
+    /// </summary>
+    /// <param name="windowID"></param>
+    private static void FillWindow(int windowID)
+    {
+        GUILayout.Label("Toggle Notifications - Toggle Notifications to be added to Community Fixes");
+        GUI.DragWindow(new Rect(0, 0, 10000, 500));
+    }
+}
