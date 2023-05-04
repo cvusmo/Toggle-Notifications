964f29cd1715682c4f278738b7f2e2d97e64850d
blacksheepcosmo <82072837+blacksheepcosmo@users.noreply.github.com>
2023-05-04 07:10:03 -0500

    Add files via upload

--- src/ToggleNotifications.cs
+++ src/ToggleNotifications.cs
@@ -13,20 +13,15 @@ using SpaceWarp.API.UI.Appbar;
 using UnityEngine;
 using System;
 using BepInEx.Logging;
-using System.Reflection;
 
 namespace ToggleNotifications
 {
     [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
     [BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
 
-    [HarmonyPatch(typeof(DiscoverableMessage))]
-    [HarmonyPatch("isActive", MethodType.Setter)]
-    [DiscoverableMessage("Events/Flight/Parts/SolarPanelsIneffectiveMessage", false, "Solar panels fully deployed but ineffective.", "")]
-
-    [HarmonyPatch(typeof(NotificationEvents))]
-    [HarmonyPatch("Update")]
-
+    //[HarmonyPatch(typeof(DiscoverableMessage))]
+    //[HarmonyPatch("isActive", MethodType.Setter)]
+    //[DiscoverableMessage("Events/Flight/Parts/SolarPanelsIneffectiveMessage")]
     public class ToggleNotificationsPlugin : BaseSpaceWarpPlugin
     {
         private ConfigEntry<bool> _enableNotificationsConfig;
@@ -35,13 +30,12 @@ namespace ToggleNotifications
         public const string ModName = "Toggle Notifications";
         public const string ModVer = "0.1.0";
 
-        private bool gameInputState = true;
-
         private const string ToolbarFlightButtonID = "BTN-ToggleNotificationsFlight";
         private const string ToolbarOABButtonID = "BTN-ToggleNotificationsOAB";
         private bool _isWindowOpen;
         private Rect _windowRect;
         private static object _solarPanelsIneffectiveNotificationHandle;
+        private static object PartIneffective;
         private bool _ToggleNotifications;
 
         public static ToggleNotificationsPlugin Instance { get; private set; }
@@ -49,106 +43,16 @@ namespace ToggleNotifications
 
         public override void OnInitialized()
         {
-            base.OnInitialized();
-            ToggleNotificationsPlugin.Instance = this;
-            this.gameInputState = GameManager.Instance.Game;
-
-            _enableNotificationsConfig = Config.Bind("Settings section", "Enable Notifications", true, "Toggle Notifications: Enabled (true) or Disabled (false)");
-            Logger.LogInfo($"Toggle Notifications Plugin: Enabled = {_enableNotificationsConfig.Value}");
-
-            // Register Flight AppBar button
-            Appbar.RegisterAppButton(
-                "Toggle Notifications",
-                ToolbarFlightButtonID,
-                AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
-                isOpen =>
-                {
-                    _isWindowOpen = isOpen;
-                    GameObject.Find(ToolbarFlightButtonID)?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(isOpen);
-                }
-            );
-
-            // Register OAB AppBar Button
-            Appbar.RegisterOABAppButton(
-                "Toggle Notifications",
-                ToolbarOABButtonID,
-                AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
-                isOpen =>
-                {
-                    _isWindowOpen = isOpen;
-                    GameObject.Find(ToolbarOABButtonID)?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(isOpen);
-                }
-            );
-
-            //variables that disable notifications by type of notification
-
-            var notificationEventsType = typeof(NotificationEvents);
-
-            //insufficientstellarexposure notification
-            var insufficientStellarExposureField = AccessTools.Field(notificationEventsType, "InsufficientStellarExposure");
-            var insufficientStellarExposureEvent = (Delegate)insufficientStellarExposureField.GetValue(null);
-            insufficientStellarExposureEvent?.DynamicInvoke(new object[] { new Action(() => { }), null });
-
-            var solarPanelsIneffectiveTimeToWaitTillField = AccessTools.Field(notificationEventsType, "_solarPanelsIneffectiveTimeToWaitTill");
-            var solarPanelsIneffectiveTimeToWaitTill = (Delegate)solarPanelsIneffectiveTimeToWaitTillField.GetValue(null);
-
-            // Check if the notification delegate is not null before trying to disable it
-            if (solarPanelsIneffectiveTimeToWaitTill != null)
-            {
-                // Set a custom time delay for the notification
-                float timeDelay = float.MaxValue; // Set your custom time delay here
-                solarPanelsIneffectiveTimeToWaitTill.DynamicInvoke(new Action(() => { }), timeDelay);
-            }
-
-            //partineffective notification
-            var partIneffectiveField = AccessTools.Field(notificationEventsType, "PartIneffective");
-            var partIneffectiveEvent = (Delegate)partIneffectiveField.GetValue(null);
-
-            // Check if the notification delegate is not null before trying to disable it
-            if (partIneffectiveEvent != null)
-            {
-                // Set a custom time delay for the notification
-                float timeDelay = float.MaxValue; // Set your custom time delay here
-                partIneffectiveEvent.DynamicInvoke(new Action(() => { }), timeDelay);
-            }
+            Instance = this;
+            _enableNotificationsConfig = Config.Bind("Settings section", "Enable Notifications", true, "Toggle Notifications: Enabled (Notifications Enabled) or Disabled (Notifications Disabled)");
+            //Logger.LogInfo($"Toggle Notifications Plugin: Enabled = {_enableNotificationsConfig.Value}");
 
             // Harmony creates the plugin/patch
             Harmony.CreateAndPatchAll(typeof(ToggleNotificationsPlugin).Assembly);
         }
         private void Update()
         {
-            //check if notications are enabled/disabled?
-            if (!_enableNotificationsConfig.Value)
-            {
-                var notificationEventsType = typeof(NotificationEvents);
-
-                // Set the handle to null if it exists
-                _solarPanelsIneffectiveNotificationHandle = null;
-
-                // Disable solar panel ineffective notification
-                var solarPanelsIneffectiveTimeToWaitTillField = AccessTools.Field(notificationEventsType, "_solarPanelsIneffectiveTimeToWaitTill");
-                var solarPanelsIneffectiveTimeToWaitTill = (Delegate)solarPanelsIneffectiveTimeToWaitTillField.GetValue(null);
-
-                // Check if the notification delegate is not null before trying to disable it
-                if (solarPanelsIneffectiveTimeToWaitTill != null)
-                {
-                    // Set a custom time delay for the notification
-                    float timeDelay = float.MaxValue; // Set your custom time delay here
-                    solarPanelsIneffectiveTimeToWaitTill.DynamicInvoke(new Action(() => { }), timeDelay);
-                }
-
-                // Disable part ineffective notification
-                var partIneffectiveField = AccessTools.Field(notificationEventsType, "PartIneffective");
-                var partIneffectiveEvent = (Delegate)partIneffectiveField.GetValue(null);
-
-                // Check if the notification delegate is not null before trying to disable it
-                if (partIneffectiveEvent != null)
-                {
-                    // Set a custom time delay for the notification
-                    float timeDelay = float.MaxValue; // Set your custom time delay here
-                    partIneffectiveEvent.DynamicInvoke(new Action(() => { }), timeDelay);
-                }
-            }
+            //figure out what to update
         }
 
         //gui not super important but would be nice to have a gui for community fixes
@@ -156,6 +60,8 @@ namespace ToggleNotifications
         {
             // Set the UI
             GUI.skin = Skins.ConsoleSkin;
+            GUI.backgroundColor = Color.white;
+            GUI.enabled = true;
 
             if (_isWindowOpen)
             {
@@ -174,10 +80,19 @@ namespace ToggleNotifications
             GUILayout.Label("Toggle Notifications - Toggle Notifications to be added to Community Fixes");
             GUI.DragWindow(new Rect(80, 20, 500, 500));
         }
-        // Method to toggle the isActive property of the DiscoverableMessage attribute
-        static void Postfix(NotificationEvents __instance)
+        // Disable SolarPanelsIneffective
+
+        //[HarmonyPatch(typeof(NotificationManager))]
+        //[HarmonyPatch("Update")]
+        //[HarmonyPatch(typeof(DiscoverableMessage))]
+        //[HarmonyPatch("Update")]
+        [HarmonyPatch(typeof(NotificationEvents))]
+        [HarmonyPatch("Update")]
+
+        static void Prefix(NotificationEvents __instance)
         {
             _solarPanelsIneffectiveNotificationHandle = null;
+            PartIneffective = null;
         }
     }
 }
