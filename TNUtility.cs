3fb89ef7768a601ef59668a3e361f9cadb92ff0b
blacksheepcosmo <82072837+blacksheepcosmo@users.noreply.github.com>
2023-05-05 14:33:25 -0500

    Add files via upload

---
+++ TNUtility.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx.Logging;
+
+namespace TNUtility;
+
+public class TNUtility
+{
+    private static ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource("ToggleNotification.Utility");
+    public static string InputDisableWindowAbbreviation = "_WindowAbbreviation";
+    public static string InputDisableWindowName = "_WindowName";
+
+}
