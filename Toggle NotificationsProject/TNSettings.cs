91ee124549ef63ba8fddc9fba3b3057598d0af25
blacksheepcosmo <82072837+blacksheepcosmo@users.noreply.github.com>
2023-05-11 21:13:54 -0500

    Update v0.2.0-release

---
+++ Toggle NotificationsProject/TNSettings.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx.Configuration;
+
+namespace ToggleNotifications
+{
+    public class TNSettings
+    {
+        private static ConfigFile s_config_file;
+        private static string s_settingspath;
+
+        public static void Init(string settingspath)
+        {
+            s_settingspath = settingspath;
+            s_config_file = new ConfigFile(settingspath, true);
+        }
+
+        public static T GetValue<T>(string key, T defaultValue)
+        {
+            return s_config_file.Bind("", key, defaultValue).Value;
+        }
+
+        public static void SetValue<T>(string key, T value)
+        {
+            s_config_file.Bind("", key, value).Value = value;
+            s_config_file.Save();
+        }
+
+        public static bool SolarPanelsIneffectiveMessage
+        {
+            get => GetValue(nameof(SolarPanelsIneffectiveMessage), true);
+            set { SetValue(nameof(SolarPanelsIneffectiveMessage), value); }
+        }
+
+        public static bool VesselLeftCommunicationRangeMessage
+        {
+            get => GetValue(nameof(VesselLeftCommunicationRangeMessage), true);
+            set { SetValue(nameof(VesselLeftCommunicationRangeMessage), value); }
+        }
+
+        public static bool VesselThrottleLockedDueToTimewarpingMessage
+        {
+            get => GetValue(nameof(VesselThrottleLockedDueToTimewarpingMessage), true);
+            set { SetValue(nameof(VesselThrottleLockedDueToTimewarpingMessage), value); }
+        }
+
+        public static bool CannotPlaceManeuverNodeWhileOutOfFuelMessage
+        {
+            get => GetValue(nameof(CannotPlaceManeuverNodeWhileOutOfFuelMessage), true);
+            set { SetValue(nameof(CannotPlaceManeuverNodeWhileOutOfFuelMessage), value); }
+        }
+
+        public static bool GamePauseToggledMessage
+        {
+            get => GetValue(nameof(GamePauseToggledMessage), true);
+            set { SetValue(nameof(GamePauseToggledMessage), value); }
+        }
+    }
+}
---
+++ Toggle NotificationsProject/TNUtility.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx.Logging;
+using ToggleNotifications;
+using ToggleNotifications.TNTools.UI;
+
+namespace ToggleNotifications
+{
+    public class TNUtility
+    {
+        private static TNUtility _instance;
+        public static TNUtility Instance { get => _instance; }
+
+        public ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource("TNUtility");
+        public static NotificationToggle currentState;
+        public static NotificationToggle UpdateCurrentStates;
+        public static string InputDisableWindowAbbreviation = "WindowAbbreviation";
+        public static string InputDisableWindowName = "WindowName";
+        public TNUtility()
+        {
+            _instance = this;
+        }
+
+        /// Refreshes the currentState.
+        public static void RefreshNotifications()
+        {
+            if (ToggleNotificationsPlugin.Instance.notificationToggle != null)
+            {
+                currentState = ToggleNotificationsPlugin.Instance.notificationToggle;
+            }
+            else
+            {
+                Instance.Logger.LogWarning("NotificationEvents is null.");
+            }
+        }
+
+        // Refreshes the currentState by calling a method in ToggleNotificationsPlugin.
+        public static void RefreshStates()
+        {
+            if (UpdateCurrentStates != null)
+            {
+                UpdateCurrentStates = ToggleNotificationsPlugin.Instance.notificationToggle;
+            }
+            else
+            {
+                Instance.Logger.LogWarning("UpdateCurrentStates is null.");
+            }
+        }
+    }
+}
