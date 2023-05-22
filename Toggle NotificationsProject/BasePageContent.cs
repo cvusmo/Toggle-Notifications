1833fe5cf01af5efdb64b0cf2a447464fe3cb7d1
cvusmo <82072837+blacksheepcosmo@users.noreply.github.com>
2023-05-22 09:41:44 -0500

    Update 0.2.2

--- Toggle NotificationsProject/BasePageContent.cs
+++ Toggle NotificationsProject/BasePageContent.cs
@@ -1,6 +1,5 @@
-using KSP.Game;
+﻿using KSP.Game;
 using KSP.Messages;
-using SpaceWarp.API.Assets;
 using ToggleNotifications.TNTools.UI;
 using UnityEngine;
 
@@ -8,45 +7,49 @@ namespace ToggleNotifications
 {
     public class BasePageContent : IPageContent
     {
-        public BasePageContent()
+        protected ToggleNotificationsPlugin mainPlugin;
+        private ToggleNotificationsUI MainUI;
+        protected NotificationToggle notificationToggle;
+        protected WindowTool windowTool;
+        private bool uiVisible;
+        private Rect windowRect = Rect.zero;
+        private int windowWidth = 250;
+        internal bool _isGUIenabled = false;
+        internal BasePageContent()
         {
-            this.MainUI = ToggleNotificationsUI.Instance;
-            this.Plugin = ToggleNotificationsPlugin.Instance;
+            this.MainUI = ToggleNotificationsUI.instance;
+            this.mainPlugin = ToggleNotificationsPlugin.Instance;
+            this.windowTool = new WindowTool();
         }
-
-        protected ToggleNotificationsPlugin Plugin;
-        protected ToggleNotificationsUI MainUI;
-        protected MessageCenterMessage Refreshing => MainUI.Refreshing;
-        protected NotificationEvents RefreshingNotification => MainUI.RefreshingNotification;
 
         public virtual string Name => throw new NotImplementedException();
         public virtual GUIContent Icon => throw new NotImplementedException();
         public bool IsRunning => false;
-        bool uiVisible;
-        public bool UIVisible { get => this.uiVisible; set => this.uiVisible = value; }
-        public virtual bool IsActive => throw new NotImplementedException();
-        MessageCenterMessage IPageContent.ConvertToMessageCenterMessage(NotificationToggle toggleState)
-        {
-            return null;
-        }
+        public bool UIVisible { get => this._isGUIenabled; set => this._isGUIenabled = value; }
+        public bool IsActive => mainPlugin._interfaceEnabled;
         public virtual void OnGUI()
         {
-            throw new NotImplementedException();
-        }
-    }
-    public class ToggleNotificationsPage : BasePageContent
-    {
-        public override string Name => "Toggle Notifications";
-        readonly Texture2D tabIcon = AssetManager.GetAsset<Texture2D>($"{ToggleNotificationsPlugin.Instance.SpaceWarpMetadata.ModID}/images/tn_icon_50.png");
-        public override GUIContent Icon => new(tabIcon, "Toggle Notifications");
-        public override bool IsActive => true;
-        //public int PageIndex { get; set; }
-        public override void OnGUI()
-        {
-            TNStyles.DrawSectionHeader("Toggle Notifications");
-            //Option.Instance.ToggleNotificationsGUI();
-            // MainUI.DrawToggleButton("Enable", NotificationType.GamePauseToggledMessage);
+            Debug.Log("OnGUI BasePageContent called");
+            if (!mainPlugin._isGUIenabled)
+                return;
+
+            GameState? gameState = mainPlugin.game?.GlobalGameState?.GetState();
+
+            if (gameState == GameState.FlightView || gameState == GameState.Map3DView || gameState == GameState.Launchpad)
+            {
+                TNBaseStyle.Init();
+                TNStyles.Init();
+
+                Texture2D imageTexture = AssetsLoader.LoadIcon("window");
+                WindowTool.CheckMainWindowPos(ref windowRect, windowWidth);
+                windowRect = GUILayout.Window(
+                    GUIUtility.GetControlID(FocusType.Passive),
+                    windowRect,
+                    MainUI.FillWindow,
+                    "<color=#696DFF>TOGGLE NOTIFICATIONS</color>"
+                );
+                mainPlugin.saverectpos();
+            }
         }
     }
 }
-
